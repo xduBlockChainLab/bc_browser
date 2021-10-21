c46368c4cb0089e00837102e406767c1ea8648b1
Cookieser <87306014+Cookieser@users.noreply.github.com>
2021-10-21 20:38:34 +0800

    Add files via upload

---
+++ src/main/java/org/xdubcl/browser/BrowserApplication.java
@@ -0,0 +1,13 @@
+package org.xdubcl.browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xdubcl/browser/BrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xdubcl.browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
